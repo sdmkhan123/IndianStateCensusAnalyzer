348de591effc834c5f50ac32aceb736be2c61bca
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-10 16:08:11 +0530

    Welcome to Indian State Census Analyzer Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyzer Problem");
+        }
+    }
+}
